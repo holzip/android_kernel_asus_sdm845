5bd8d29354418a30852c348cfee46caecd9413b7
holzip <crutojzaur2011@gmail.com>
2026-04-02 12:49:34 +0300

    + build.sh

---
+++ build.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# Compile script for Cuh kernel
+# Copyright (C) 2020-2023 Adithya R.
+# Copyright (C) 2023-2026 Tejas Singh.
+# Copyright (C) 2026 holzip.
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="depressive-kernel-Z01R-$(TZ=Europe/Moscow date +"%Y%m%d-%H%M").zip"
+TC_DIR="$HOME/tc/prelude-clang"
+GCC_64_DIR="$HOME/tc/aarch64-linux-android-4.9"
+GCC_32_DIR="$HOME/tc/arm-linux-androideabi-4.9"
+AK3_DIR="AnyKernel3"
+DEFCONFIG="sdm845-perf_defconfig vendor/asus/Z01R.config vendor/debugfs.config"
+export PATH="$TC_DIR/bin:$PATH"
+
+# Build Environment
+#sudo -E apt-get -qq update
+#sudo -E apt-get -qq install bc python2 python3 python-is-python3
+
+# Check for essentials
+if ! [ -d "${TC_DIR}" ]; then
+echo "Clang not found! Cloning to ${TC_DIR}..."
+if ! git clone --depth=1 https://gitlab.com/jjpprrrr/prelude-clang.git -b master ${TC_DIR}; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+if ! [ -d "${GCC_64_DIR}" ]; then
+echo "gcc not found! Cloning to ${GCC_64_DIR}..."
+if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git ${GCC_64_DIR}; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+if ! [ -d "${GCC_32_DIR}" ]; then
+echo "gcc_32 not found! Cloning to ${GCC_32_DIR}..."
+if ! git clone --depth=1 -b lineage-19.1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git ${GCC_32_DIR}; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+cp out/defconfig arch/arm64/configs/$DEFCONFIG
+exit
+fi
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 \
+    CC=clang \
+    LD=ld.lld \
+    AR=llvm-ar \
+    NM=llvm-nm \
+    OBJCOPY=llvm-objcopy \
+    OBJDUMP=llvm-objdump \
+    STRIP=llvm-strip \
+    LLVM_IAS=1 \
+    CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    Image.gz-dtb
+
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ]; then
+    echo -e "\nKernel compiled successfully! Zipping up...\n"
+    
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3/
+    
+    cd AnyKernel3
+    zip -r9 "../$ZIPNAME" * -x .git* README.md *placeholder
+    cd ..
+    
+    echo -e "\n###############################################"
+    echo -e "  DONE! Your zip: $ZIPNAME"
+    echo -e "###############################################"
+else
+    echo -e "\nCompilation failed! Image.gz-dtb not found."
+    exit 1
+fi
